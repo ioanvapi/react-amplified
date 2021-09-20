9963681faa5a756f89923eb0896887dbd35d4464
vapirovschi <vapirovschi@arezzosky.com>
2021-09-20 19:33:54 +0300

    fetch api added

--- src/App.js
+++ src/App.js
@@ -4,16 +4,30 @@ import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifyGreetings } from '@aws-amplify/ui-react'
 import { onAuthUIStateChange } from "@aws-amplify/ui-components";
+import Amplify, { API } from 'aws-amplify';
 
 
 function App() {
   const [user, setUser] = useState();
+  const [message, setMessage] = useState("No message yet !!!");
 
   useEffect(() => {
     return onAuthUIStateChange((nextAuthState, authData) => {
       setUser(authData)
     });
   }, [])
+
+  const handleClick = async () => {
+    const resp = await getData()
+    setMessage(resp)
+    console.log('REsponse:', resp)
+  }
+
+  const getData = async () => {
+    const apiName = 'todos';
+    const path = '/items'; 
+    return await API.get(apiName, path)
+  }
 
   return (
     <div className="App">   
@@ -23,6 +37,8 @@ function App() {
         <p>
           Hello from React Amplified - Backend Changed!!!
         </p>
+        <button name='Ceva' onClick={() => handleClick()}>Submit</button>
+        <p>{message}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
